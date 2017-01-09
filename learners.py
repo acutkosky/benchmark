d2c4208aa6825ee634e147078da2b09557f462b0
Ashok Cutkosky <ashok@DN0a2215af.SUNet>
2017-01-09 13:10:46 -0800

    admit that k is a hyperparameter

--- learners.py
+++ learners.py
@@ -64,18 +64,18 @@ class AdaGrad(bm.Learner):
         return ['D']
 
 
-def freeexp_diag_reg(w, scaling):
+def freeexp_diag_reg(w, scaling, k):
     '''regularizer used for diagonal freeexp'''
     abs_w = np.abs(w*scaling)
-    return np.sqrt(FREEEXP_K)*((abs_w + 1)*np.log(abs_w + 1) - abs_w)
+    return k * np.sqrt(FREEEXP_K)*((abs_w + 1)*np.log(abs_w + 1) - abs_w)
 
-def freeexp_sphere_reg(w):
+def freeexp_sphere_reg(w, k):
     ''''regularizer used for l2 freeexp'''
     norm_w = np.linalg.norm(w)
-    return np.sqrt(FREEEXP_K)*((norm_w + 1)*np.log(norm_w + 1) - norm_w)
+    return k * np.sqrt(FREEEXP_K)*((norm_w + 1)*np.log(norm_w + 1) - norm_w)
 
 def update_learning_rate_sphere(accumulated_regret, old_L, one_over_eta_squared, \
-    weights, gradient, gradients_sum, psi):
+    weights, gradient, gradients_sum, k, psi):
     '''Computes aggressive learning rate updates by measuring
     discrepencies between regret bounds.
     Will increase learning rates without compromising worst-case
@@ -99,9 +99,9 @@ def update_learning_rate_sphere(accumulated_regret, old_L, one_over_eta_squared,
         old_L * gradients_sum_norm))
 
     new_weights_plus_max = - (gradients_sum)/(gradients_sum_norm + EPSILON) \
-        * (np.exp(gradients_sum_norm/(np.sqrt(FREEEXP_K) * one_over_eta_plus_max)) - 1)
+        * (np.exp(gradients_sum_norm/(k * np.sqrt(FREEEXP_K) * one_over_eta_plus_max)) - 1)
     new_weights_plus_min = - (gradients_sum)/(gradients_sum_norm + EPSILON) \
-        * (np.exp(gradients_sum_norm/(np.sqrt(FREEEXP_K) * one_over_eta_plus_min)) - 1)
+        * (np.exp(gradients_sum_norm/(k * np.sqrt(FREEEXP_K) * one_over_eta_plus_min)) - 1)
 
     accumulated_regret_max = accumulated_regret \
         + (np.sqrt(one_over_eta_squared) - one_over_eta_plus_max) * psi(new_weights_plus_max) \
@@ -123,7 +123,7 @@ def update_learning_rate_sphere(accumulated_regret, old_L, one_over_eta_squared,
     return new_accumulated_regret, new_one_over_eta_squared
 
 def update_learning_rate_diag(accumulated_regret, old_L, one_over_eta_squared, \
-    weights, gradient, gradients_sum, scaling, psi):
+    weights, gradient, gradients_sum, scaling, k, psi):
     '''Computes aggressive learning rate updates by measuring
     discrepencies between regret bounds.
     Will increase learning rates without compromising worst-case
@@ -145,9 +145,9 @@ def update_learning_rate_diag(accumulated_regret, old_L, one_over_eta_squared, \
         old_L * gradients_sum_norm))
 
     new_weights_plus_max = -np.sign(gradients_sum)/scaling \
-        * (np.exp(gradients_sum_norm/(np.sqrt(FREEEXP_K) * one_over_eta_plus_max)) - 1)
+        * (np.exp(gradients_sum_norm/(k * np.sqrt(FREEEXP_K) * one_over_eta_plus_max)) - 1)
     new_weights_plus_min = -np.sign(gradients_sum)/scaling \
-        * (np.exp(gradients_sum_norm/(np.sqrt(FREEEXP_K) * one_over_eta_plus_min)) - 1)
+        * (np.exp(gradients_sum_norm/(k * np.sqrt(FREEEXP_K) * one_over_eta_plus_min)) - 1)
 
     accumulated_regret_max = accumulated_regret \
         + (np.sqrt(one_over_eta_squared) - one_over_eta_plus_max) * psi(new_weights_plus_max) \
@@ -172,19 +172,23 @@ def update_learning_rate_diag(accumulated_regret, old_L, one_over_eta_squared, \
 class FreeExpSphere(bm.Learner):
     '''L2 FreeExp Learner'''
     def __init__(self, shape, hyperparameters=None):
-        hyperparameters = None
+        if hyperparameters is None:
+            hyperparameters = {'k': 1.0}
+        hyperparameters = {'k': hyperparameters['k']}
+
         super(FreeExpSphere, self).__init__('FreeExpSphere')
 
         self.one_over_eta_squared = EPSILON
         self.one_over_eta_squared_without_increases = 0
 
+        self.k = hyperparameters['k']
         self.L = 0
         self.gradients_sum = 0
         self.accumulated_regret = 0
 
         self.parameter = np.zeros(shape)
 
-        self.psi = freeexp_sphere_reg
+        self.psi = lambda w: freeexp_sphere_reg(w, self.k)
 
     def update(self, loss_info):
         '''update parameters'''
@@ -198,7 +202,7 @@ class FreeExpSphere(bm.Learner):
         self.accumulated_regret, new_one_over_eta_squared = \
             update_learning_rate_sphere(self.accumulated_regret, \
                 self.L, self.one_over_eta_squared, \
-                self.parameter, gradient, self.gradients_sum, self.psi)
+                self.parameter, gradient, self.gradients_sum, self.k, self.psi)
 
         self.L = np.maximum(self.L, grad_norm)
 
@@ -209,7 +213,7 @@ class FreeExpSphere(bm.Learner):
         self.one_over_eta_squared = new_one_over_eta_squared
 
         self.parameter = - self.gradients_sum/(gradients_sum_norm + EPSILON) \
-            * (np.exp(gradients_sum_norm/np.sqrt(FREEEXP_K * self.one_over_eta_squared)) - 1)
+            * (np.exp(gradients_sum_norm/(self.k * np.sqrt(self.one_over_eta_squared))) - 1)
 
     def get_status(self):
         '''return a printable string describing the status of the learner'''
@@ -220,15 +224,22 @@ class FreeExpSphere(bm.Learner):
                 np.sqrt(self.one_over_eta_squared_without_increases))
         return default_string + ' ' + increasing_learning_rates
 
+    @staticmethod
+    def hyperparameter_names():
+        return ['k']
+
 class FreeExpDiag(bm.Learner):
     '''diagonal FreeExp Learner'''
     def __init__(self, shape, hyperparameters=None):
-        hyperparameters = None
+        if hyperparameters is None:
+            hyperparameters = {'k': 1.0}
+        hyperparameters = {'k': hyperparameters['k']}
         super(FreeExpDiag, self).__init__('FreeExpDiag', hyperparameters)
 
         self.one_over_eta_squared = np.zeros(shape) + EPSILON
         self.one_over_eta_squared_without_increases = np.zeros(shape)
 
+        self.k - hyperparameters['k']
         self.L = np.zeros(shape)
         self.gradients_sum = np.zeros(shape)
         self.accumulated_regret = np.zeros(shape)
@@ -237,7 +248,7 @@ class FreeExpDiag(bm.Learner):
 
         self.scaling = np.ones(shape)
 
-        self.psi = lambda weights: freeexp_diag_reg(weights, self.scaling)
+        self.psi = lambda weights: freeexp_diag_reg(weights, self.scaling, self.k)
 
     def update(self, loss_info):
         '''update parameters'''
@@ -251,7 +262,7 @@ class FreeExpDiag(bm.Learner):
         self.accumulated_regret, new_one_over_eta_squared = \
             update_learning_rate_diag(self.accumulated_regret, \
                 self.L, self.one_over_eta_squared, \
-                self.parameter, gradient, self.gradients_sum, self.scaling, self.psi)
+                self.parameter, gradient, self.gradients_sum, self.scaling, self.psi, self.k)
 
         self.L = np.maximum(self.L, np.abs(gradient))
 
@@ -262,7 +273,7 @@ class FreeExpDiag(bm.Learner):
         self.one_over_eta_squared = new_one_over_eta_squared
 
         self.parameter = -np.sign(self.gradients_sum)/self.scaling \
-            * (np.exp(gradients_sum_norm/np.sqrt(FREEEXP_K * self.one_over_eta_squared)) - 1)
+            * (np.exp(gradients_sum_norm/(self.k * np.sqrt(self.one_over_eta_squared))) - 1)
 
     def get_status(self):
         '''return a printable string describing the status of the learner'''
@@ -273,16 +284,26 @@ class FreeExpDiag(bm.Learner):
                 np.average(np.sqrt(self.one_over_eta_squared_without_increases)))
         return default_string + ' ' + increasing_learning_rates
 
+    @staticmethod
+    def hyperparameter_names():
+        return ['k']
+
 class FreeExpScaledFeatures(FreeExpDiag):
     '''FreeExp that scales features to be theoretically robust to many measurements.'''
 
     def __init__(self, shape, hyperparameters=None):
-        hyperparameters = None
+        if hyperparameters is None:
+            hyperparameters = {'k': 1.0}
+        hyperparameters = {'k': hyperparameters['k']}
         super(FreeExpScaledFeatures, self).__init__('FreeExpScaledFeatures', hyperparameters)
 
         self.scaling = np.reshape(np.arange(1, 1+len(self.parameter.flatten())), shape)
         self.scaling = self.scaling * np.log(self.scaling + 1)
-        self.psi = lambda weights: freeexp_diag_reg(weights, self.scaling)
+        self.psi = lambda weights: freeexp_diag_reg(weights, self.scaling, self.k)
+
+    @staticmethod
+    def hyperparameter_names():
+        return ['k']
 
 class PiSTOLSphere(bm.Learner):
     '''PiSTOL spherical learner'''
